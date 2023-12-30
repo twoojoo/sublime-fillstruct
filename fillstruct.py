2dce7db0d3c871342a9603e57776ebc80cf345eb
Giovanni Campeol <91201182+twoojoo@users.noreply.github.com>
2023-12-30 09:06:01 +0100

    Create fillstruct.py

---
+++ fillstruct.py
@@ -0,0 +1,25 @@
+import sublime
+import sublime_plugin
+import subprocess
+import json
+
+
+class FillstructCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		pos = self.view.sel()[0].begin();
+    
+		cmd = "fillstruct -file " + self.view.file_name() + " -offset " + str(pos)
+		# print("[fillstruct debug] running command: " + cmd)
+
+		output = ""
+		try:
+			output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+			# print("[fillstruct debug] command output: " + output)
+		except subprocess.CalledProcessError as e:
+		    print(f'Error running fillstruct: {e}'); sublime.status_message(f'Error running fillstruct: {e}'); return;
+		
+		output = json.loads(output)
+		content = output[0]["code"] #just pick the code section
+		content = content.split("{", 1)[1][:-1] #remove struct name and curly braces
+
+		self.view.insert(edit, pos, content)
